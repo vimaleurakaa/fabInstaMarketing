0c2a8f825e22f1b93729c99e2a5737f32a7f2bee
harshithmb <harshith05@gmail.com>
2021-03-17 15:14:22 +0530

    Updates

---
+++ precache-manifest.8e4b67e38d34ef6dc9dc2f644f2c40bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d511a9c0043e957adc1e2dfc7894338",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "49879b02eaff04b1f5e1",
+    "url": "/e-commerce/static/css/2.fc3ea6fc.chunk.css"
+  },
+  {
+    "revision": "9eb40bcb46040eade97b",
+    "url": "/e-commerce/static/css/main.4560ea98.chunk.css"
+  },
+  {
+    "revision": "49879b02eaff04b1f5e1",
+    "url": "/e-commerce/static/js/2.2811f79c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-commerce/static/js/2.2811f79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb40bcb46040eade97b",
+    "url": "/e-commerce/static/js/main.0b87c9fe.chunk.js"
+  },
+  {
+    "revision": "cdbddee13da6df33b0db",
+    "url": "/e-commerce/static/js/runtime-main.f3af71a0.js"
+  }
+]);
